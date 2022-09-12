3ace82c3315979a77c2a753833fe9219e84a08c3
Georg Bauer <bauer@rohmann.de>
2022-09-12 11:13:49 +0200

    add script to load svf

--- tools/jtag/loadsvf.py
+++ tools/jtag/loadsvf.py
@@ -1,13 +1,26 @@
 #! /usr/bin/env python
-from discover import driver, config
+from playtag.lib.userconfig import UserConfig
+from playtag.lib.userconfig import basic_startup
 from playtag.svf import runsvf
+
+class SvfFileError(Exception):
+    """Svf error"""
 
 class SvfDefaults(object):
     SVF = None
 
+#config = UserConfig()
+config = basic_startup()
+
+if config.SHOW_CONFIG:
+    print(config.dump())
+
 config.add_defaults(SvfDefaults)
+
+config.SVF = 'simple_example.svf'
 
 if not config.SVF:
     print('Expected SVF=<fname>')
+    raise SvfFileError("Please add a filename!")
 
-runsvf(config.SVF, driver)
+runsvf(config.SVF, config.driver)
